d5b2c8be7992c724c002f8e9900d5083cf957ac6
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2022-12-03 15:44:29 +0530

    Almost done need to set local storage option so we can see items even after refresh the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import Item from './components/Item';
 import './App.css';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [EditId, setEditId] = useState(null);
+  const itemRef = useRef();
+  const itemSubmitHandler = (e) => {
+    e.preventDefault();
+    let item = itemRef.current.value.trim();
+    if (item.length === 0) {
+      return
+    }
+    if (isEditing) {
+      setItems(prev => prev.map(data => {
+        if (data.id === EditId) {
+          return { ...data, item: item }
+        }
+        return data;
+      }))
+      setValue("");
+      setIsEditing(false)
+      return;
+    }
+    let id = Math.floor(Math.random() * 1000);
+    setItems(prev => [{ id: id + 1, item: item }, ...prev]);
+    setValue("");
+  }
+  const onChangeHandler = (e) => {
+    setValue(e.target.value)
+  }
+  const handleDelete = (id) => {
+    setItems(prev => prev.filter(item => item.id !== id));
+  }
+  const handleEdit = (item, id) => {
+    setIsEditing(true);
+    setValue(item);
+    setEditId(id);
+
+  }
   return (
     <main className="App">
       <section className='Card'>
         <h3 className="heading">Grocery List</h3>
-        <form action="">
-          <input type="text" />
-          <button type='submit'>Add</button>
+        <form onSubmit={itemSubmitHandler}>
+          <input
+            ref={itemRef}
+            type="text"
+            value={value}
+            onChange={onChangeHandler}
+          />
+          <button type='submit'>{isEditing ? "Edit" : "Add"}</button>
         </form>
-        <Item />
+        {items.map(item =>
+          <Item
+            key={item.id}
+            id={item.id}
+            item={item.item}
+            delete={handleDelete}
+            edit={handleEdit}
+          />)}
       </section>
     </main>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
 import { FaEdit } from 'react-icons/fa'
 import { MdDelete } from 'react-icons/md'
 import styles from './Item.module.css'
-const Item = () => {
+const Item = (props) => {
+    const onDeleteHandler = () => {
+        props.delete(props.id);
+    }
+    const onEditHandler = () => {
+        props.edit(props.item, props.id);
+    }
     return (
         <article className={styles.ItemCard}>
-            <p>Item</p>
+            <p>{props.item}</p>
             <div className={styles.btn}>
-                <FaEdit className={styles.edit} />
-                <MdDelete className={styles.delete} />
+                <FaEdit onClick={onEditHandler} className={`${styles.edit} ${styles.icon}`} />
+                <MdDelete onClick={onDeleteHandler} className={`${styles.delete} ${styles.icon}`} />
             </div>
         </article >
     )
